40106e5ca4256ce757c1e21a399eb068352d3637
praxidike97 <fabianstern97@gmail.com>
2024-08-27 21:19:31 +0200

    Small changes

--- src/SignGeneratorGUI/python/webserver.py
+++ src/SignGeneratorGUI/python/webserver.py
@@ -13,5 +13,9 @@ def single_converter(eva_nr):
     generate_timetable_from_eva_nr(int(eva_nr), "/home/pi/Projects/TrainDisplay/src/SignGeneratorGUI/python/credentials.json")
     return "Generated"
 
+@app.get('/test/<number1>/<number2>')
+def single_converter(number1, number2):
+    return str(number1) + ":" + str(number2)
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
